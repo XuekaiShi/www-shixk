319f20842b7f52922fc8a84ea0e05a4af15e5c1b
ShiXuekai <ShiXuekai2002@outlook.com>
2024-02-06 21:43:54 +0800

    feat: 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFound from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +25,7 @@ const router = createRouter({
       name: 'notes',
       component: () => import('../views/NotesView.vue')
     },
+    { path: '/:pathMatch(.*)', component: NotFound }
   ]
 })
 
