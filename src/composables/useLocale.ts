8c3d3bfa49555ef1088dce1a041632ab7b9b290a
ShiXuekai <ShiXuekai2002@outlook.com>
2024-02-08 00:30:56 +0800

    feat: useLocale could change html:lang

--- src/composables/useLocale.ts
+++ src/composables/useLocale.ts
@@ -1,3 +1,4 @@
+import { watchEffect } from "vue"
 import { useLocalStorage } from "@vueuse/core";
 import i18n from "@/i18n";
 const { locale } = i18n.global
@@ -7,6 +8,11 @@ const LOCALE_KEY = '__locale__'
 type LocaleType = typeof locale.value
 
 locale.value = localStorage.getItem(LOCALE_KEY) as LocaleType || 'en'
+document.documentElement.lang = localStorage.getItem(LOCALE_KEY) as LocaleType || 'en'
+
+watchEffect(() => {
+  document.documentElement.lang = locale.value
+})
 
 useLocalStorage(LOCALE_KEY, locale)
 
