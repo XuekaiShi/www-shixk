6520aa064322a70fc74348d9e9e962a056ffb721
ShiXuekai <ShiXuekai2002@outlook.com>
2024-02-08 01:15:59 +0800

    feat: style of 404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,3 +1,45 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import IconTerminalLine from '@/components/icons/IconTerminalLine.vue';
+const router = useRouter()
+const goTo = (path: string) => {
+  router.push(path)
+}
+</script>
 <template>
-  404 NotFound
+  <h2>{{ $t('notfound.heading') }}</h2>
+  <div class="item" @click="goTo('/')">
+    <i>
+      <IconTerminalLine />
+    </i>
+    <span>
+      cd ..
+    </span>
+  </div>
 </template>
+<style scoped>
+h2 {
+  margin-bottom: 2rem;
+}
+
+.item {
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+  align-items: center;
+  /* background-color: red */
+}
+
+span {
+  margin-left: 0.2em;
+}
+
+i {
+  color: var(--color-text);
+  display: flex;
+  place-items: center;
+  place-content: center;
+  width: 1em;
+  height: 1em;
+}
+</style>
