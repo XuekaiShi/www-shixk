25a4bad5930b4fa4992e3d6e967eeb54d7559b59
ShiXuekai <ShiXuekai2002@outlook.com>
2024-08-30 22:17:53 +0800

    feat: bottom border for list

--- src/components/list/List.vue
+++ src/components/list/List.vue
@@ -25,7 +25,7 @@ const filterList = list.filter((item) => {
 </script>
 
 <template>
-  <div>
+  <div class="list">
     <ListItem v-for="item in filterList" :key="item.id" @click="goTo(item.id, item.category)">
       <template #title>
         {{ item.title }}
@@ -40,5 +40,41 @@ const filterList = list.filter((item) => {
         {{ item.abstract }}
       </template>
     </ListItem>
+    <ListItem v-for="item in filterList" :key="item.id" @click="goTo(item.id, item.category)">
+      <template #title>
+        {{ item.title }}
+      </template>
+      <template #category>
+        {{ item.category }}
+      </template>
+      <template #date>
+        {{ item.date }}
+      </template>
+      <template #abstract>
+        {{ item.abstract }}
+      </template>
+    </ListItem>     <ListItem v-for="item in filterList" :key="item.id" @click="goTo(item.id, item.category)">
+      <template #title>
+        {{ item.title }}
+      </template>
+      <template #category>
+        {{ item.category }}
+      </template>
+      <template #date>
+        {{ item.date }}
+      </template>
+      <template #abstract>
+        {{ item.abstract }}
+      </template>
+    </ListItem>
+
   </div>
 </template>
+
+<style scoped>
+.list {
+  min-height: calc(100vh - var(--app-padding) * 2 - 8rem);
+  border-bottom: 1.5px solid var(--color-text);
+  margin-bottom: var(--app-padding);
+}
+</style>
--- src/views/list/PostsView.vue
+++ src/views/list/PostsView.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
 import ListHeader from '@/components/list/ListHeader.vue';
 import List from '@/components/list/List.vue';
+import Affix from '@/components/Affix.vue';
 </script>
 <template>
-  <ListHeader></ListHeader>
-  <List></List>
+  <div class="list-view">
+    <ListHeader></ListHeader>
+    <List></List>
+    <!-- <Affix></Affix> -->
+  </div>
 </template>
+<style scoped>
+.list-view {
+  width: 100%;
+  position: relative;
+}
+</style>
